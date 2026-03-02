623bf8002f379afbf5f76de8509ad0611517deb1
Ihab Khaled <ihab.khaled94@gmail.com>
2026-03-02 02:13:26 +0200

    chore: add commitlint with conventional commits enforcement

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,8 +1,20 @@
-const buildTscCommand = () => `npm run typecheck`
-
-const buildLintCommand = () => `npm run lint:fix`
+const buildTscCommand = () => 'npm run typecheck'
+const buildLintCommand = () => 'npm run lint:fix'
+const buildPrettierCommand = () => 'npm run format'
 
 module.exports = {
-  '*.{ts,tsx,js,jsx}': [buildLintCommand, buildTscCommand],
-  '*.{ts,tsx,js,jsx,json,css,md,yml,yaml}': ['prettier --write'],
+  '*.{ts,tsx,js,jsx}': (files) => {
+    if (!files.length) return []
+
+    return [
+      buildLintCommand(),
+      buildTscCommand(),
+    ]
+  },
+
+  '*.{ts,tsx,js,jsx,json,css,md,yml,yaml}': (files) => {
+    if (!files.length) return []
+
+    return [buildPrettierCommand()]
+  },
 }
